7270c22ed6adc561cf20cc2b84586a032860655e
Ubuntu <jay@sfia2.hh4ilrhtztkezmeljydfqfqhjg.zx.internal.cloudapp.net>
2020-08-11 08:07:28 +0000

    init script working

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 sudo apt update -y &> /dev/null
 sudo apt install -y git &> /dev/null
 
-curl https://get.docker.com &> /dev/null | sudo bash &> /dev/null
+curl https://get.docker.com | sudo bash &> /dev/null
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &> /dev/null
 sudo chmod +x /usr/local/bin/docker-compose &> /dev/null
 
@@ -11,7 +11,7 @@ export DB_HOST=mysql:3306
 
 git clone https://github.com/jordan-grindrod/flask-sfia-2-example flask-sfia-2-example &> /dev/null && cd $_ &> /dev/null
 
-docker-compose up -d &> /dev/null
+sudo docker-compose up -d &> /dev/null
 
 cd .. && rm -rf flask-sfia-2-example &> /dev/null
 
