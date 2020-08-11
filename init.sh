5e1766477c11a11b74e4773b81e9a9f430d3e88b
Ubuntu <jay@sfia2.hh4ilrhtztkezmeljydfqfqhjg.zx.internal.cloudapp.net>
2020-08-11 07:50:05 +0000

    working on init script

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-sudo apt update -y
-sudo apt install -y git
+sudo apt update -y &> /dev/null
+sudo apt install -y git &> /dev/null
 
-curl https://get.docker.com | sudo bash
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl https://get.docker.com &> /dev/null | sudo bash &> /dev/null
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &> /dev/null
+sudo chmod +x /usr/local/bin/docker-compose &> /dev/null
 
 export DB_HOST=mysql:3306
 
-git clone https://github.com/jordan-grindrod/flask-sfia-2-example flask-sfia-2-example && cd $_
+git clone https://github.com/jordan-grindrod/flask-sfia-2-example flask-sfia-2-example &> /dev/null && cd $_ &> /dev/null
 
-docker-compose up -d > /dev/null
+docker-compose up -d &> /dev/null
 
-cd .. && rm -rf flask-sfia-2-example
+cd .. && rm -rf flask-sfia-2-example &> /dev/null
 
 echo "Application is running on localhost"
