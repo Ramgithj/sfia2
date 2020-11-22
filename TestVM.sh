21b83bd7874e971bff144c2089431dfb2b4a6caa
Ramgithj <71395269+Ramgithj@users.noreply.github.com>
2020-11-22 19:21:36 +0000

    Update TestVM.sh

--- TestVM.sh
+++ TestVM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh -i /var/lib/jenkins/id_rsa ubuntu@54.247.4.200 << EOF
-sudo rm-r sfia2
+sudo rm -r sfia2
 git clone https://github.com/Ramgithj/sfia2.git
 cd sfia2/
 export DATABASE_URI=mysql+pymysql://admin:radiatorspoon102@testdb.cvuavhfwkpq2.eu-west-1.rds.amazonaws.com/users
