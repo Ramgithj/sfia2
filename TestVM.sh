7844c2489885f2fae61d3bf5e1f744ab20f1939f
Ramgithj <71395269+Ramgithj@users.noreply.github.com>
2020-11-22 18:21:56 +0000

    Update TestVM.sh

--- TestVM.sh
+++ TestVM.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-
 ssh -i /var/lib/jenkins/id_rsa ubuntu@54.247.4.200 << EOF
-
 if [ ! -d sfia2 ]
 then
     git clone https://github.com/Ramgithj/sfia2.git
 fi
-
 cd sfia2/
 export DATABASE_URI=mysql+pymysql://admin:radiatorspoon102@testdb.cvuavhfwkpq2.eu-west-1.rds.amazonaws.com/users
 export TEST_DATABASE_URI=mysql+pymysql://admin:radiatorspoon102@testdb.cvuavhfwkpq2.eu-west-1.rds.amazonaws.com/testdb
 docker-compose up -d
 echo "completed"
 docker exec backend bash -c "pytest tests/ --cov application" > backend-report.txt
-
 docker exec frontend bash -c "pytest tests/ --cov application" > frontend-report.txt
 docker-compose down
 EOF
