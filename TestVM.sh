4eaf6b52dec3f45aa88103d5e42674beb373f606
Ramgithj <71395269+Ramgithj@users.noreply.github.com>
2020-11-22 17:26:07 +0000

    Update TestVM.sh

--- TestVM.sh
+++ TestVM.sh
@@ -10,8 +10,8 @@ fi
 cd sfia2/
 export DATABASE_URI=mysql+pymysql://admin:radiatorspoon102@testdb.cvuavhfwkpq2.eu-west-1.rds.amazonaws.com/users
 export TEST_DATABASE_URI=mysql+pymysql://admin:radiatorspoon102@testdb.cvuavhfwkpq2.eu-west-1.rds.amazonaws.com/testdb
-docker-compose up -d --build'
-docker exec backend bash -c "pytest tests/ --cov application" > backend-report.txt'
-docker exec frontend bash -c "pytest tests/ --cov application" > frontend-report.txt'
+docker-compose up -d --build
+docker exec backend bash -c "pytest tests/ --cov application" > backend-report.txt
+docker exec frontend bash -c "pytest tests/ --cov application" > frontend-report.txt
 docker-compose down
 EOF
